07acf05476e2649b868f8f3fcc0dae34ceb7e903
youssef karam <1020118@stemegypt.edu.eg>
2025-04-20 23:13:54 +0200

    test git

---
+++ mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowState(Qt::WindowMaximized);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_loginButton_clicked()
+{
+    QString user = ui->user->text();
+    if(user == "yoyo"){
+        QMessageBox::information(this,"Alert","Login Done Successfully");
+    }
+}
+
